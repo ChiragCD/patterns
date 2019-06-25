fe0f66dd9c8e7f1ce62d79572a9d353ea79a7e8d
ChiragCD <Chirag8CD@gmail.com>
2019-06-25 18:57:16 +0530

    Basic idea rectangular

--- rectangular.py
+++ rectangular.py
@@ -1,5 +1,10 @@
 import pygame, random
 
+"""
+Movers move in compass directions and randomly create nodes and reproduce there, moving off
+in various compass directions. Trace their paths as they go.
+"""
+
 class mover(object):
 
     def __init__(self):
