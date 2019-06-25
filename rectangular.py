1341190af4e18744bf4a48251497f4c70797cb12
ChiragCD <Chirag8CD@gmail.com>
2019-06-25 18:54:53 +0530

    Start new patterns

---
+++ rectangular.py
@@ -0,0 +1,14 @@
+import pygame, random
+
+class mover(object):
+
+    def __init__(self):
+
+        pass
+
+class node(object):
+
+    def __init__(self, position):
+
+        self.x = position[0]
+        self.y = position[1]
